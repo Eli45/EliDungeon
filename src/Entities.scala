8ecd3bce8374f20ccf999b41636830cd24135f5b
Eli45 <elifurland@gmail.com>
2014-12-01 20:14:57 -0500

    Organize into folders.

---
+++ src/Entities.scala
@@ -0,0 +1,36 @@
+object Entities extends App {
+	import publicVariables._
+	/* ---- LOW LEVEL ---- */
+	def Slime =	{
+		mHP = 13;
+		mDAM = 6;
+	}
+	def Lizard =	{
+		mHP = 6;
+		mDAM = 8;
+	}
+	/* ---- /LOW LEVEL ---- */
+	
+	/* ---- MID LEVEL ---- */
+		//Add a mid level monster?
+	/* ---- /MID LEVEL ---- */
+	
+	/* ---- HIGH LEVEL ---- */
+	def Drake =	{
+		mHP = 35;
+		mDAM = 17;
+	}
+	def Matt =	{
+		mHP = 50;
+		mDAM = 13;
+	}
+	/* ---- /HIGH LEVEL ---- */
+	
+	/* ---- BOSS ---- */
+	def KingKelman =	{
+		mHP = 100;
+		mDAM = 15;
+	}
+	/* ---- /BOSS ---- */
+
+}
